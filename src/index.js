aa2c2402d1c3348efcbf9bc52ba6d8d96210a2fc
jamal westfield <jamalwestfield@Jamals-MacBook-Pro.local>
2018-09-13 11:04:37 +0100

    chracter count working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const formElement = document.querySelector("form")
 const textBoxElement = document.querySelector(".user-text")
 const timelineElement = document.querySelector(".timeline")
 const characterCounterElement = document.querySelector(".character-counter")
+const submitButtonElement = document.querySelector(".submit-button")
+
+
+
 formElement.addEventListener("submit", event => {
   event.preventDefault()
   console.log("default prevented")
@@ -13,12 +17,22 @@ formElement.addEventListener("submit", event => {
   timelineElement.insertBefore(newTweet, latestTweet)
   textBoxElement.value = ""
   characterCounterElement.textContent = `0 / 280`
+  characterCounterElement.style.color = "black";
 })
 
 textBoxElement.addEventListener("input", event => {
   const currentCount = event.target.value.length
   characterCounterElement.textContent = `${currentCount} / 280`
+  if(currentCount > 280){
+      characterCounterElement.style.color = "red"
+      submitButtonElement.disabled = true 
+  }else {
+      submitButtonElement.disabled = false
+      characterCounterElement.style.color ="black"
+  }
 })
+
+
 
 
 
