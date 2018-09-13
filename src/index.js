3b1f4e481a0e80b564df1bf4a6a80f6d952e6399
jamal westfield <jamalwestfield@Jamals-MacBook-Pro.local>
2018-09-13 14:33:33 +0100

    type to tweet

--- src/index.js
+++ src/index.js
@@ -3,55 +3,68 @@ const textBoxElement = document.querySelector(".user-text")
 const timelineElement = document.querySelector(".timeline")
 const characterCounterElement = document.querySelector(".character-counter")
 const submitButtonElement = document.querySelector(".submit-button")
+submitButtonElement.disabled = true // all submit button changes contribute to submit button actions
+characterCounterElement.textContent = `Type To Tweet something..`
 
-//when submit is clicked, add tweet to timeline
-formElement.addEventListener("submit", event => {
-  event.preventDefault()
-  console.log("default prevented")
-  const latestTweet = document.querySelector(".latest-tweet")
-  const userInput = textBoxElement.value
+user();;
+characterCounter();
 
 
 
 
-  const newTweet = document.createElement("div")
-  newTweet.setAttribute("class", "latest-tweet")
-  newTweet.textContent = userInput
 
-  const linkRegex = /(?:|[ ])@([a-zA-Z]+)/g
+function user() {
+  //when submit is clicked, add tweet to timeline
+  formElement.addEventListener("submit", event => {
+    event.preventDefault()
+    console.log("default prevented")
+    const latestTweet = document.querySelector(".latest-tweet")
+    const userInput = textBoxElement.value
+    
 
 
-  newTweet.innerHTML = newTweet.textContent.replace(linkRegex, ` <a href="/@$1">@$1</a>`)
 
-  const deleteIconElement = document.createElement("button")
-  deleteIconElement.textContent = "delete"
-  newTweet.appendChild(deleteIconElement)
-  deleteIconElement.addEventListener("click", event => {
-    timelineElement.removeChild(newTweet)
+    const newTweet = document.createElement("div")
+    newTweet.setAttribute("class", "latest-tweet")
+    newTweet.textContent = userInput
+
+    const linkRegex = /(?:|[ ])@([a-zA-Z]+)/g
+    newTweet.innerHTML = newTweet.textContent.replace(linkRegex, ` <a href="/@$1">@$1</a>`)
+
+    const deleteIconElement = document.createElement("button")
+    deleteIconElement.textContent = "delete"
+    newTweet.appendChild(deleteIconElement)
+    deleteIconElement.addEventListener("click", event => {
+      timelineElement.removeChild(newTweet)
+    })
+
+
+    timelineElement.insertBefore(newTweet, latestTweet)
+    textBoxElement.value = ""
+    characterCounterElement.textContent = `0 / 280`
+    characterCounterElement.style.color = "black"
+    submitButtonElement.disabled = true
   })
-
-
-  timelineElement.insertBefore(newTweet, latestTweet)
-  textBoxElement.value = ""
-  characterCounterElement.textContent = `0 / 280`
-  characterCounterElement.style.color = "black"
-})
-
+}
 
 
 //updates character counter
-textBoxElement.addEventListener("input", event => {
-  const currentCount = event.target.value.length
-  characterCounterElement.textContent = `${currentCount} / 280`
-  if (currentCount > 280){
-    characterCounterElement.style.color = "red"
-    submitButtonElement.disabled = true
-  }else{
-    characterCounterElement.style.color = "black"
-    submitButtonElement.disabled = false
-  }
+function characterCounter() {
+  textBoxElement.addEventListener("input", event => {
+    const currentCount = event.target.value.length
+    characterCounterElement.textContent = `${currentCount} / 280`
+    if (currentCount > 280 || currentCount < 1) {
+      characterCounterElement.style.color = "red"
+      submitButtonElement.disabled = true
+      characterCounterElement.textContent = `Type To Tweet something..`
+    }
+    else {
+      characterCounterElement.style.color = "black"
+      submitButtonElement.disabled = false
+    }
+  })
+}
 
-})
 
 
 
